a036d48f29485df288da5937c90d7d747c1b6fbe
codyb1150 <codyb1150@gmail.com>
2019-05-06 14:15:50 -0400

    Add project files.

---
+++ CarMeetFinderMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarMeetFinderMVC.Startup))]
+namespace CarMeetFinderMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
